80bbf61f7be6b836ff5d036c95f7dc7514f12290
Ayako <ayakoo4646@gmail.com>
2025-05-07 14:28:54 +0200

    Prepare for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ScrollToTop } from "./shared/ScrollToTop.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<ScrollToTop />
-				<App />
-			</QueryClientProvider>
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <HashRouter>
+      <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
+        <App />
+      </QueryClientProvider>
+    </HashRouter>
+  </StrictMode>,
 );
